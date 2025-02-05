00135eb17699508b5942ebc9682eb418efb34ab5
maruyama-s1 <maruyamas.work@gmail.com>
2025-02-05 10:46:03 +0900

    Viteの設定-Bootstrap、css、jsファイルをまとめて管理

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/styles.scss', 'resources/js/app.js', 'resources/css/style.css'],
+            // input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
